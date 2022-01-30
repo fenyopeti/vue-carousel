caf05de7909579dd9320ba8f65d5bb9ffb9fa542
Peter Fenyvesi <peter.fenyvesi@emarsys.com>
2022-01-30 13:25:32 +0100

    init

---
+++ README.md
@@ -0,0 +1,24 @@
+# vue-carousel
+
+## Project setup
+```
+npm install --save @fenyopeti/vue-carousel
+```
+
+## Import
+```
+import VueCarousel from '@fenyopeti/vue-carousel'
+```
+
+
+### Install Globally
+```
+Vue.use(VueCarousel)
+```
+
+### Use Components Locally
+```
+import VueCarousel from '@fenyopeti/vue-carousel'
+```
+
+
---
+++ src/install.js
@@ -0,0 +1,16 @@
+import VueCarousel from "./components/VueCarousel.vue";
+
+const VueCarouselSimple = {
+ install(Vue, options) {
+  // Let's register our component globally
+  // https://vuejs.org/v2/guide/components-registration.html
+  Vue.component("vue-carousel", VueCarousel);
+ }
+};
+
+// Automatic installation if Vue has been added to the global scope.
+if (typeof window !== 'undefined' && window.Vue) {
+    window.Vue.use(VueCarouselSimple);
+}
+
+export default VueCarouselSimple;
